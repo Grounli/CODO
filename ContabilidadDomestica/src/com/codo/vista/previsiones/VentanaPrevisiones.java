fba4f469fbf78bb02a3dc8f3bc1a1f344dd40aa6
Gregory Negrette <gregory1negrette@gmail.com>
2017-04-28 23:38:45 +0200

    Previsiones base

--- ContabilidadDomestica/src/com/codo/vista/previsiones/VentanaPrevisiones.java
+++ ContabilidadDomestica/src/com/codo/vista/previsiones/VentanaPrevisiones.java
@@ -1,43 +1,98 @@
 package com.codo.vista.previsiones;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
 
 import com.codo.controlador.ControladorPrevisiones;
 import com.codo.modelo.pojos.Previsiones;
+import com.codo.vista.componentes.ModeloTablaMovimientos;
 import com.codo.vista.interfaces.InterfazPrevisiones;
 
 public class VentanaPrevisiones extends JDialog implements InterfazPrevisiones {
 
-	private final JPanel contentPanel = new JPanel();
+
+	private static final long serialVersionUID = 1L;
 	
+	private static final int POSICION_HORIZONTAL = 100;
+	private static final int POSICION_VERTICAL = 100;
+	private static final int ANCHURA_MAXIMA = 500;
+	private static final int ALTURA_MAXIMA = 400;
+	private static final int ANCHURA_MINIMA_PANEL_COMPONENTES = 500;
+	private static final int ALTURA_MINIMA_PANEL_COMPONENTES = 35;
+	private final JPanel contentPanel = new JPanel();
+	private JTable tablaMovimientos;
+	private JScrollPane panelDesplazamiento;
+	private ModeloTablaMovimientos mtp;
+	private JButton btnAnadir;
+	private JButton btnBorrar;
+	private JButton btnModificar;
+	private JButton btnEjecutar;
+	private JButton btnSalir;
+
 	public VentanaPrevisiones(List<Previsiones> listaDePrevisiones) {
 		setTitle("Previsiones");
-		setBounds(100, 100, 450, 300);
+		setBounds(POSICION_HORIZONTAL, POSICION_VERTICAL, ANCHURA_MAXIMA, ALTURA_MAXIMA);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panelComponentes = new JPanel();
+			panelComponentes.setBounds(0, 0, ANCHURA_MINIMA_PANEL_COMPONENTES, ALTURA_MINIMA_PANEL_COMPONENTES);
+			panelComponentes.setPreferredSize(new Dimension (ANCHURA_MINIMA_PANEL_COMPONENTES,ALTURA_MINIMA_PANEL_COMPONENTES));
+			panelComponentes.setMinimumSize(new Dimension (ANCHURA_MINIMA_PANEL_COMPONENTES,ALTURA_MINIMA_PANEL_COMPONENTES));
+			contentPanel.add(panelComponentes, BorderLayout.SOUTH);
+			panelComponentes.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+			{
+				btnAnadir = new JButton("Añadir");
+				panelComponentes.add(btnAnadir);
+			}
+			{
+				btnBorrar = new JButton("Borrar");
+				panelComponentes.add(btnBorrar);
+			}
+			{
+				btnModificar = new JButton("Modificar");
+				panelComponentes.add(btnModificar);
+			}
+			{
+				btnEjecutar = new JButton("Ejecutar");
+				panelComponentes.add(btnEjecutar);
+			}
+		}
+		{
+			
+			// CONSTRUCCION DE TABLA
+			tablaMovimientos = new JTable();
+			tablaMovimientos.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+			tablaMovimientos.setFillsViewportHeight(false);
+			tablaMovimientos.setRowHeight(30);
+			Font f = new Font("Time New Roman", Font.PLAIN, 12);
+			tablaMovimientos.setFont(f);
+
+			panelDesplazamiento = new JScrollPane(tablaMovimientos);
+			panelDesplazamiento.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+			panelDesplazamiento.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+			contentPanel.add(panelDesplazamiento, BorderLayout.CENTER);
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				JButton btnSalir = new JButton("Salir");
+				btnSalir.setActionCommand("Cancel");
+				buttonPane.add(btnSalir);
 			}
 		}
 	}
